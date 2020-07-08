dfba89505ec3291d05677bf4d25447b9d6611899
fatma <fatma_saal@ens.univ-artois.fr>
2020-07-08 15:37:16 +0200

    Initial commit

---
+++ CountdownConsole/Data/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CountdownConsole.Data;
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Countdown");
+            Console.WriteLine(TimerCD.countDTimer());
+        }
+    }
+}
---
+++ CountdownConsole/Data/TimerCD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Timers;
+namespace CountdownConsole.Data
+{
+
+
+    class TimerCD
+    {
+        public TimerCD()
+        {
+            countDTimer();
+        }
+
+        public static int number = 2000;
+        public static bool countDTimer()
+        {
+            Timer tm = new Timer();
+            tm.Interval = 1000;
+            tm.Elapsed += (s, e) => number -= 10;
+            number -= 10;
+            tm.Start();
+            while (number > 0)
+            {
+                Console.WriteLine(number);
+            }
+            tm.Stop();
+            return true;
+        }
+
+    }
+
+}
